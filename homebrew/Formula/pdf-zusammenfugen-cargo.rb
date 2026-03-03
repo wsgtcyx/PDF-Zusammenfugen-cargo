b00ad6b7ab6c25e3263ae83dc5de50f042f91868
wsgtcyx <439248005@qq.com>
2026-03-03 15:54:04 +0800

    chore: set formula sha256 for v0.1.0

--- homebrew/Formula/pdf-zusammenfugen-cargo.rb
+++ homebrew/Formula/pdf-zusammenfugen-cargo.rb
@@ -2,7 +2,7 @@ class PdfZusammenfugenCargo < Formula
   desc "Rust-Tool zum pdf zusammenfuegen, pdf verbinden und pdf mergen"
   homepage "https://github.com/wsgtcyx/PDF-Zusammenfugen-cargo"
   url "https://github.com/wsgtcyx/PDF-Zusammenfugen-cargo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "8d6115b1eb62ee484ad28ce7184f8e4184508c34ea31b4595857319ece6d3710"
   license "MIT"
 
   depends_on "rust" => :build
